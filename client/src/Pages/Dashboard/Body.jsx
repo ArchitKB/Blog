2f37a807439a455a2c3d5b9627968cb384a14485
ArchitKB <architkumarbhardwaj15@gmail.com>
2024-01-29 18:23:58 +0530

    likes and comments setup

--- client/src/Pages/Dashboard/Body.jsx
+++ client/src/Pages/Dashboard/Body.jsx
@@ -1,6 +1,9 @@
 import React,{useState, useEffect} from "react";
 import axios from 'axios';
 import './Dashboard.css';
+import LikedButton from '@mui/icons-material/Favorite';
+import LikeButton from '@mui/icons-material/FavoriteBorder'
+import CommentButton from '@mui/icons-material/Comment';
 // import Userwidget from 
 
 function Profile(){
@@ -31,10 +34,25 @@ function Feed(){
         fetchPost();
     }, []);
 
-    const userId = JSON.parse(localStorage.getItem('user'));
+    const userId = JSON.parse(localStorage.getItem('user'))._id;
 
-    
-    const postsArray = posts.map((post, index) => (<Post key = {index} userName = {post.firstName + ' ' + post.lastName}  description = {post.description} likes={Object.keys(post.likes).length} is_liked={post.likes.userId} image ={post.picturePath} />));
+    const postsArray = posts.map((post) => {
+        let likes = 0;
+        let is_liked = false;
+
+        Object.entries(post.likes).map( ([key, values]) => {
+            if(key === userId && values === true){
+                is_liked = true;
+            }
+
+            if(values === true){
+                likes++;
+            }
+        });
+
+
+        return <Post key = {post._id} id = {post._id} userName = {post.firstName + ' ' + post.lastName}  description = {post.description} likes={likes} is_liked={is_liked} image ={post.picturePath} />
+    });
 
 
     return(
@@ -48,11 +66,42 @@ function Post(props){
 
     const userName = props.userName;
     const description = props.description;
-    return(<li>
-        <h1>{userName}</h1>
-        <p>{description}</p>
-        <p>Likes: {props.likes}</p>
+    
+    const [isLiked , setIsLiked] = useState(props.is_liked);
+    const [likes, setLikes] = useState(props.likes);
+    async function handleLike (){
+        if(isLiked){
+            setIsLiked(false);
+            setLikes(likes-1);
+        }
 
+        else{
+            setIsLiked(true);
+            setLikes(likes+1);
+        }
+        
+        const userId = JSON.parse(localStorage.getItem('user'))._id;
+        try{
+            await axios.patch( `http://localhost:3001/posts/${props.id}/like`,{ userId : userId}, {
+                        headers: {
+                            'Authorization' : "Bearer " + localStorage.getItem('token').slice(1,-1),
+                            'Content-Type' : 'application/json',
+                        },
+                    });
+        } catch(error){
+            console.log(error);
+        }
+    }
+
+    async function handleComment(){
+
+    }
+
+    return(<li>
+        <h2>{userName}</h2>
+        <p>{description}</p>
+        <p>Likes: {likes}</p>
+        <p>{isLiked? "true" : "false"}</p>
         <img
         style={{ objectFit: "cover", borderRadius: "" }}
         width={400}
@@ -60,7 +109,8 @@ function Post(props){
         alt="user"
         src={`http://localhost:3001/assets/${props.image}`}
       />
-        <p>{props.is_liked === undefined? false:true}</p>
+        <p>{isLiked === true? <LikedButton onClick={handleLike} sx={{color:'red'}}/>:<LikeButton onClick={handleLike} sx={{color:'red'}}/>}</p>
+        <p><CommentButton sx={{color:'greenyellow'}} onClick={handleComment}/></p>
     </li>);
 }
 
